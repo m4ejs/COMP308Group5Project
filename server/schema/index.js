49a19919b342ae91d60092944ecd06d863a1838b
Karan Handa <khanda4@my.centennialcollege.ca>
2025-04-13 17:54:54 -0400

    Remove backend code for clean feature-based setup

--- server/schema/index.js
+++
@@ -1,8 +0,0 @@
-import { GraphQLSchema } from 'graphql';
-import RootQuery from './queries/index.js';
-import Mutation from './mutations/index.js';
-
-export default new GraphQLSchema({
-  query: RootQuery,
-  mutation: Mutation
-});
